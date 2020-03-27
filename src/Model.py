e1ff9f365634e94ee9b044b84e99792c3fbb21d0
markansn <manson10@hotmail.co.uk>
2020-03-27 21:15:24 +0000

    moved file extraction into new repository + removed redundant code:

---
+++ src/Model.py
@@ -0,0 +1,328 @@
+import io
+
+import requests, json, os, pdf2image, sys
+from PIL import Image
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+import platform
+#use brew to install popper on mac
+#CODE BY YANSONG LIU
+# global Vars
+
+# Add your Computer Vision subscription key and endpoint to your environment variables.
+authsubscription_key = ""
+authendpoint = ""
+authocr_url = ""
+
+# dicts
+generalDict = {}
+keywordDict = {}
+
+
+def assaginaAuthenticationCredentials(subsKey, endPoint, ocr_url):
+    global authsubscription_key
+    global authendpoint
+    global authocr_url
+    authsubscription_key = subsKey
+    authendpoint = endPoint
+    authocr_url = ocr_url
+
+
+def authenticateClient():
+    credentials = CognitiveServicesCredentials(authsubscription_key)
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=authendpoint, credentials=credentials)
+    return text_analytics_client
+
+
+def key_phrases(doc):
+    client = authenticateClient()
+
+    # print('keyphase', ConvertToJSON(doc))
+    try:
+        response = client.key_phrases(documents=json.loads(ConvertToJSON(doc)))
+        return response.documents
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+def ConvertToJSON(OCRout):
+    temp = json.dumps(eval(str(OCRout)))
+    return str(temp)
+
+
+def parseStringToInt(array1, array2):
+    for i in range(len(array1)):
+        array1[i] = int(array1[i])
+    for i in range(len(array2)):
+        array2[i] = int(array2[i])
+    return array1, array2
+
+
+def inTheSameParagraph(array1, array2):
+    array1, array2 = parseStringToInt(array1, array2)
+
+    limit = 30
+
+    leftBondA1 = array1[0]
+    topBondA1 = array1[1]
+    widthA1 = array1[2]
+    heightA1 = array1[3]
+
+    leftBondA2 = array2[0]
+    topBondA2 = array2[1]
+    widthA2 = array2[2]
+    heightA2 = array2[3]
+
+    if abs((leftBondA1 + widthA1) - leftBondA2) < limit:
+        return True
+    elif abs((leftBondA2 + widthA2) - leftBondA1) < limit:
+        return True
+    elif abs((topBondA1 + heightA1) - topBondA2) < limit:
+        return True
+    elif abs((topBondA2 + heightA2) - topBondA1) < limit:
+        return True
+    else:
+        return False
+
+
+def OCR2KeyPhrasesReFormatter(OCRout):
+    outputString = ""
+    outputBuffer = []
+    id = 0
+
+    plainText = json.loads(OCRout)
+    regions = plainText['regions']
+    for region in regions:
+        id += 1
+        lines = region['lines']
+        for line in lines:
+            words = line['words']
+            for word in words:
+                text = word.get('text')
+                outputString += text + " "
+        temp = json.dumps({'id': id, 'language': 'en', 'text': outputString})
+        outputBuffer.append(json.loads(temp))
+    return outputBuffer
+
+
+def callForEachWord(dict, json_text):
+    json_dict = json.loads(json_text)
+    regions = json_dict['regions']
+
+    for region in regions:
+        lines = region['lines']
+        for line in lines:
+            words = line['words']
+            for word in words:
+                increaseCounter(dict, word['text'])
+
+
+def callForEachWordScore(dict, json_text):
+    json_dict = json.loads(json_text)
+    regions = json_dict['regions']
+    scoreDel = 0;
+
+    for region in regions:
+        lines = region['lines']
+        for line in lines:
+            words = line['words']
+            for word in words:
+                if (search_dict(dict, word['text'])):
+                    scoreDel += 5;
+    return scoreDel
+
+
+def search_dict(inputdict, key):
+    if key in inputdict:
+        return True
+    return False
+
+
+def increaseCounter(inputdict, key):
+    if (search_dict(inputdict, key)):
+        inputdict[key] += 1
+    else:
+        inputdict[key] = 1
+
+
+def getTopValue(dictionary, num_of_items):
+    sorted_dict = {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1], reverse=True)}
+
+    first_x_pairs = {k: sorted_dict[k] for k in list(sorted_dict)[:num_of_items]}
+
+    return first_x_pairs
+
+
+def OCR(image): #!!!
+    # Read the image into a byte array
+    image_data = image_to_byte_array(image)
+
+    headers = {'Ocp-Apim-Subscription-Key': authsubscription_key, 'Content-Type': 'application/octet-stream'}
+    params = {'language': 'unk', 'detectOrientation': 'true'}
+    data = image_data
+    response = requests.post(authocr_url, headers=headers, params=params, data=data)
+    response.raise_for_status()
+
+    return response
+
+
+def callForEachWordKey(dict, json_text):
+    json_text = ConvertToJSON(json_text.replace("None", "\"None\""))
+    json_dict = json.loads(ConvertToJSON(json_text))
+    key_phrases = json_dict['key_phrases']
+    for phrase in key_phrases:
+        for word in phrase.split():
+            increaseCounter(dict, word)
+
+
+def callForEachWordKeyReal(dict, json_text):
+    json_text = ConvertToJSON(json_text.replace("None", "\"None\""))
+    json_dict = json.loads(ConvertToJSON(json_text))
+    key_phrases = json_dict['key_phrases']
+    scoreDel = 0
+    for phrase in key_phrases:
+        if search_dict(dict, phrase):
+            scoreDel += 1
+    return scoreDel
+
+
+def callForEachWordRealOriginal(inputdict, json_text):
+    json_dict = json.loads(json_text)
+    regions = json_dict['regions']
+    scoreDel = 0
+
+    for region in regions:
+        lines = region['lines']
+        for line in lines:
+            words = line['words']
+            for word in words:
+                if (search_dict(inputdict, word['text'])):
+                    scoreDel += 0.5
+    return scoreDel
+
+
+def getLength(OCRout):
+    length = 0
+    outputString = ""
+    outputBuffer = []
+    id = 0
+
+    plainText = json.loads(OCRout)
+    regions = plainText['regions']
+    for region in regions:
+        id += 1
+        lines = region['lines']
+        for line in lines:
+            words = line['words']
+            for word in words:
+                length += 1
+        temp = json.dumps({'id': id, 'language': 'en', 'text': outputString})
+        outputBuffer.append(json.loads(temp))
+    return length
+
+
+def trainAlgo(filepath):
+    global generalDict
+    global keywordDict
+    trainingPath = filepath
+    print(trainingPath)
+    analysis = OCR(trainingPath).json() #!!!
+    analysis = ConvertToJSON(analysis)
+    analyisReformedForKeyPhases = OCR2KeyPhrasesReFormatter(analysis)
+    keywordsList = key_phrases(analyisReformedForKeyPhases)
+
+    # General
+    callForEachWord(generalDict, analysis)
+    generalDict = getTopValue(generalDict, 200)
+    # Key
+    for keywords in keywordsList:
+        callForEachWordKey(keywordDict, str(keywords))
+    # if keywordsList != None:
+    #     for keywords in keywordsList:
+    #         callForEachWordKey(keywordDict, str(keywords))
+    # else:
+    #     filepath.show()
+    #     print("stop")
+
+    keywordDict = getTopValue(keywordDict, 200)
+
+
+def extractAlgo(filepath):
+    score = 0
+    percentage = 0
+    realDataPath = filepath
+    new_analysis = OCR(realDataPath).json()
+    new_analysis = ConvertToJSON(new_analysis)
+    analyisReformedForKeyPhasesReal = OCR2KeyPhrasesReFormatter(new_analysis)
+    keywordsListReal = key_phrases(analyisReformedForKeyPhasesReal)
+
+    for keywordsReal in keywordsListReal:
+        score += callForEachWordKeyReal(keywordDict, str(keywordsReal))
+    generalScore = 0
+    generalScore += callForEachWordRealOriginal(generalDict, new_analysis)
+
+    score += generalScore
+    length = getLength(new_analysis)
+    percentage += (score / length) * 100
+    if score>130:
+        filepath.show()
+    return score
+
+
+def listPdfs(filepath):
+    directory = filepath
+    pdfs = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
+            pdfs.append(os.path.join(directory, filename))
+        else:
+            continue
+    return pdfs
+
+
+def image_to_byte_array(image: Image):
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format=image.format)
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+
+def pdfsIterator(pdfs): #returns list of list of images from pdf !!
+    pdfStream = []
+    pathname = os.path.dirname(sys.argv[0])
+    if platform.system() == 'Windows':
+        pathname = os.path.abspath(pathname) + os.path.sep + "poppler-0.68.0" + os.path.sep + "bin"
+
+    for pdf in pdfs:
+
+        if platform.system() == 'Windows':
+            pages = pdf2image.convert_from_path(pdf, 200, poppler_path=pathname, fmt="jpeg")
+        else:
+            pages = pdf2image.convert_from_path(pdf, 200, fmt="jpeg")
+
+        pdfStream.append(pages)
+
+    return pdfStream
+
+
+def trainingWithPath(inputFolder):
+    pdfs = listPdfs(inputFolder)
+    pdfStream = pdfsIterator(pdfs)
+    for pdf in pdfStream:
+        for page in pdf:
+            trainAlgo(page)
+
+
+def extractWithPath(inputFolder):
+    result = []
+    pdfs = listPdfs(inputFolder)
+    pdfStream = pdfsIterator(pdfs)
+    for pdf in pdfStream:
+        score = []
+        pageNum = 1
+        for page in pdf:
+            score.append(str(pageNum)+':'+str(extractAlgo(page)))
+            pageNum += 1
+    result.append(score)
+    return result
---
+++ src/extract.py
@@ -0,0 +1,297 @@
+import Model
+import glob
+import json
+import string
+from io import BytesIO
+from PIL import Image
+import pickle
+import pymysql
+from tqdm import tqdm
+# import nltk
+# from nltk.corpus import words as nltk_words
+# nltk.download('words')
+# dictionary = dict.fromkeys(nltk_words.words(), None)
+
+# def is_english_word(word): #credit https://stackoverflow.com/questions/3788870/how-to-check-if-a-word-is-an-english-word-with-python
+#
+#     try:
+#         x = dictionary[word]
+#         return True
+#     except KeyError:
+#         return False
+
+
+setofwords = set(line.strip() for line in open(
+	'allEnglishWords.txt'))  # credit https://stackoverflow.com/questions/874017/python-load-words-from-file-into-a-set
+punctuation = str.maketrans(dict.fromkeys(string.punctuation)) #credit https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string
+
+
+connection = pymysql.connect(host='ancssc-db.mysql.database.azure.com',
+                             user='ancssc@ancssc-db',
+                             password='819UiC@Uj&$Z^GY',
+                             db='team_36_db_do_not_touch',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+
+
+def getImgSize(img):
+	return createImageBuffer(img).tell()
+
+def createImageBuffer(img):
+	img_file = BytesIO()
+	img.save(img_file, 'jpeg', quality=90)
+	return img_file
+
+def reduceImageSize(img):
+
+	img_file = createImageBuffer(img)
+	return Image.open(img_file)
+
+
+def getTextFromImage(img):
+	text = []
+
+
+	img_file = BytesIO()
+	img.save(img_file, 'jpeg')
+	img_file_size = img_file.tell() #credit  https://stackoverflow.com/questions/11904083/how-to-get-image-size-bytes-using-pil
+
+
+	if img_file_size < 1000000:
+
+
+		img = Image.open(img_file)
+
+		data = Model.OCR(img).json()
+
+		data = Model.ConvertToJSON(data)
+
+		json_dict = json.loads(data)
+		regions = json_dict['regions']
+
+		for region in regions:
+			lines = region['lines']
+			for line in lines:
+				words = line['words']
+				for word in words:
+					text.append(word['text'])
+		return text
+	else:
+		print("!Warn: file too big to read")
+
+	return []
+
+
+
+def getWordsOnPage(page, images, word_exists):
+	text = []
+	first_page = None
+	try:
+		first_page = getTextFromImage(images[0][page])
+	except:
+		return []
+
+	# for img in images[0]:
+	#
+	#     text = getTextFromImage(img)
+
+	words_on_first_page = []
+	banned_words = ["annual", "report", "", " ", "results", "reports"]
+	for word in first_page:
+		# print(word)
+		#print(word)
+		# if '@' in word.lower() or "www." in word.lower() or ".org" in word.lower():
+		#     print(word)
+
+
+		word = word.lower().translate(punctuation)
+
+		# if not word_exists:
+		#
+		#     if word not in setofwords and not word.isdigit():
+		#         words_on_first_page.append(word)
+		# else:
+		#     if not word.isdigit:
+		#         words_on_first_page.append(word)
+		if word not in banned_words:
+			words_on_first_page.append(word)
+	return words_on_first_page
+
+
+def possible_name_in_file_name(word, reportName):
+	return word.lower() in reportName.lower()
+
+def get_matching_words_from_urls(images, words_on_first_page):
+	words_matching_urls = []
+
+	images[0].reverse()
+	for img in images[0]:
+		text = getTextFromImage(img)
+
+		urls = []
+		for word in text:
+			if '@' in word.lower():
+				split_word = word.split("@")
+				# if word.lower() in split_word[1]:
+				#     urls.append(word)
+				urls.append(split_word[1])
+			elif "www." in word.lower() or ".org" in word.lower():
+				split_word = word.split(".")
+				if split_word[0] == "www" or "http" in split_word[0]:
+					urls.append(split_word[1])
+				else:
+					urls.append(word)
+
+		for url in urls:
+			# print(url)
+			for possible_name in words_on_first_page:
+				if possible_name in url and len(possible_name) > 3:
+					words_matching_urls.append(possible_name)
+
+			if words_matching_urls != []:
+				return words_matching_urls
+
+	return []
+
+def get_matching_words_from_file_name(words_on_first_page, reportName):
+	words_in_file_name = []
+	for word in words_on_first_page:
+		if possible_name_in_file_name(word, reportName) and not word.isdigit():
+			words_in_file_name.append(word)
+
+	if words_in_file_name != []:
+		return words_in_file_name
+
+	return []
+
+
+def concat_words(i):
+
+	string = ""
+	for item in i:
+		string = string + item + " "
+	return string
+
+def readReport(reportName):
+	images = Model.pdfsIterator([reportName])
+
+	words_on_first_page = getWordsOnPage(0, images, True) + getWordsOnPage(1, images, True) + getWordsOnPage(2, images, True)
+
+	words_on_first_page = list(dict.fromkeys(words_on_first_page))
+
+	# if len(words_on_first_page) == 0:
+	#     return "no names found on first page"
+
+
+
+	# print(words_on_first_page)
+
+	matching_words_from_urls = get_matching_words_from_urls(images, words_on_first_page)
+
+	if matching_words_from_urls == []:
+		matching_words_from_file_name = get_matching_words_from_file_name(words_on_first_page, reportName)
+
+		if matching_words_from_urls != []:
+			return concat_words(matching_words_from_file_name), words_on_first_page
+
+	if matching_words_from_urls != []:
+		return concat_words(matching_words_from_urls), words_on_first_page
+
+
+
+	return "none", words_on_first_page
+
+
+
+
+
+
+	# return "found " + str(words_on_first_page) + " but none could be verified"
+
+
+def upload_pdf(file, answer):
+	try:
+		with connection.cursor() as cursor:
+			# Create a new record
+			sql = "INSERT INTO `pdfs` (`PDF_NAME`, `NGO_NAME`) VALUES (%s, %s)"
+			cursor.execute(sql, (file, answer))
+		connection.commit()
+
+	except:
+		print("ERR")
+
+def get_pdfs_in_db():
+	with connection.cursor() as cursor:
+		# Read a single record
+		sql = "SELECT `PDF_NAME` FROM `pdfs`"
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		# print(result)
+
+	results  = []
+
+	for r in result:
+		results.append(r["PDF_NAME"])
+
+	return results
+
+
+def main():
+	configurationFile = open("configuration.txt", "r")
+	subscription_key = configurationFile.readline().rstrip("\n\r")
+	endpoint = configurationFile.readline().rstrip("\n\r")
+	configurationFile.close()
+	ocr_url = endpoint + "vision/v2.1/ocr"
+
+	Model.assaginaAuthenticationCredentials(subscription_key, endpoint, ocr_url)
+
+
+
+	database_items = get_pdfs_in_db()
+	print(database_items)
+	files = glob.glob("../reports/*.pdf")
+	for file in files:
+		file_name = file.replace("../reports/", "")
+
+		if file_name not in database_items:
+			print("------")
+			print(file_name)
+			print("searching...  ")
+			p = readReport(file)
+			name = p[0]
+
+			words = p[1]
+
+			answer = "-"
+
+			out = name
+
+			print(name)
+			while answer != "":
+				if answer == "a":
+					s = ""
+					for word in words:
+						s += word + " "
+						print(s)
+				elif answer == "n":
+					break
+				elif answer != "-":
+					out = answer
+					break
+				answer = input("confirm/replace > ")
+
+			if answer != "n":
+				upload_pdf(file_name, out)
+
+
+
+
+	connection.close()
+
+
+
+
+main()
+
