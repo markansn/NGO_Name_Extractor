e872320fe09312c572698cfe9d6a61a8d02f6e0c
markansn <manson10@hotmail.co.uk>
2020-04-02 21:24:00 +0100

    final

--- src/connect.py
+++ src/connect.py
@@ -1,7 +1,7 @@
 import pymysql
-connection = pymysql.connect(host='ancssc-db.mysql.database.azure.com',
-                             user='ancssc@ancssc-db',
-                             password='819UiC@Uj&$Z^GY',
-                             db='team_36_db_do_not_touch',
+connection = pymysql.connect(host='',
+                             user='',
+                             password='',
+                             db='',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
